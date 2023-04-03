3d58dc02a420819dc940ac2de4f984f3c6624470
fajar <fajar@DESKTOP-FE6Q96B>
2023-04-03 21:27:21 +0700

    Add project files.

---
+++ db_restoran/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace db_restoran
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            string constring = "server= localhost; database= db_restoran; uid= root; pwd= Fajar12BuDiman";
+            string query = "";
+
+            MySqlConnection con = new MySqlConnection(constring);
+            MySqlCommand cmd = new MySqlCommand(query, con);
+
+            MySqlDataReader read;
+            try
+            {
+            con.Open();
+            read = cmd.ExecuteReader();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
